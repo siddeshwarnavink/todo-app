72eaa968abc21a03af4c626ebb3db7a8f94d039d
Siddeshwar <jjsiddeshwar61@gmail.com>
2019-03-02 15:36:27 +0530

    Preparing axios for dep

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9999"
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:9999"
+      : "http://server.todo-app.siddeshrocks.in/"
 });
 
 export default instance;
