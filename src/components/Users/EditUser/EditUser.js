b3ae1170908d64886c6edb124b0b8d8b538c9831
Siddeshwar <jjsiddeshwar61@gmail.com>
2019-04-26 08:46:59 +0530

    Added change password

--- src/components/Users/EditUser/EditUser.js
+++ src/components/Users/EditUser/EditUser.js
@@ -1,166 +1,193 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import axios from "../../../axios";
-import classes from "./EditUser.module.css";
-import checkValidation from "../../../utility/checkValidation";
+import axios from '../../../axios';
+import classes from './EditUser.module.css';
+import checkValidation from '../../../utility/checkValidation';
 
-import Button from "../../UI/Button/Button";
-import Input from "../../UI/Input/Input";
-import Spinner from "../../UI/Spinner/Spinner";
+import Button from '../../UI/Button/Button';
+import Input from '../../UI/Input/Input';
+import Spinner from '../../UI/Spinner/Spinner';
 
 class EditUser extends Component {
-  state = {
-    controls: {
-      username: {
-        elementType: "input",
-        elementConfig: {
-          label: "Username",
-          type: "text",
-          placeholder: "e.g: Dr.RoX"
-        },
-        value: this.props.currentUser.username,
-        validation: {
-          required: true
-        },
-        valid: false,
-        touched: false
-      }
-    }
-  };
+     state = {
+          controls: {
+               username: {
+                    elementType: 'input',
+                    elementConfig: {
+                         label: 'Username',
+                         type: 'text',
+                         placeholder: 'e.g: Dr.RoX',
+                    },
+                    value: this.props.currentUser.username,
+                    validation: {
+                         required: true,
+                    },
+                    valid: false,
+                    touched: false,
+               },
 
-  componentDidMount() {
-    if (this.props.isAdmin) {
-      const isAdminField = {
-        elementType: "checkbox",
-        elementConfig: {
-          label: "Is Admin"
-        },
-        value: this.props.currentUser.isAdmin,
-        valid: true,
-        touched: true
-      };
+               password: {
+                    elementType: 'input',
+                    elementConfig: {
+                         label: 'New Password',
+                         type: 'text',
+                         placeholder: 'e.g: letmein123',
+                    },
+                    value: '',
+                    validation: {
+                         required: false,
+                    },
+                    valid: false,
+                    touched: false,
+               },
+          },
+     };
 
-      this.setState(prvState => ({
-        controls: {
-          ...prvState.controls,
-          isAdminField
-        }
-      }));
-    }
-  }
+     componentDidMount() {
+          if (this.props.isAdmin) {
+               const isAdminField = {
+                    elementType: 'checkbox',
+                    elementConfig: {
+                         label: 'Is Admin',
+                    },
+                    value: this.props.currentUser.isAdmin,
+                    valid: true,
+                    touched: true,
+               };
 
-  inputChangedHandler = (event, controlName) => {
-    const updatedControls = {
-      ...this.state.controls,
-      [controlName]: {
-        ...this.state.controls[controlName],
-        value: event.target.value,
-        valid: checkValidation(
-          event.target.value,
-          this.state.controls[controlName].validation
-        ),
-        touched: true
-      }
-    };
+               this.setState(prvState => ({
+                    controls: {
+                         ...prvState.controls,
+                         isAdminField,
+                    },
+               }));
+          }
+     }
 
-    this.setState({
-      controls: updatedControls
-    });
-  };
+     inputChangedHandler = (event, controlName) => {
+          const updatedControls = {
+               ...this.state.controls,
+               [controlName]: {
+                    ...this.state.controls[controlName],
+                    value: event.target.value,
+                    valid: checkValidation(
+                         event.target.value,
+                         this.state.controls[controlName].validation,
+                    ),
+                    touched: true,
+               },
+          };
 
-  submitHandler = event => {
-    event.preventDefault();
-    axios
-      .post(
-        `/?token=${localStorage.getItem("token")}`,
-        JSON.stringify({
-          query: `
+          this.setState({
+               controls: updatedControls,
+          });
+     };
+
+     submitHandler = event => {
+          event.preventDefault();
+          axios.post(
+               `/?token=${localStorage.getItem('token')}`,
+               JSON.stringify({
+                    query: `
             mutation {
               editUser(userId: ${this.props.userId}, username: "${
-            this.state.controls.username.value
-          }", isAdmin:${
-            this.state.controls.isAdminField
-              ? this.state.controls.isAdminField.value
-              : null
-          }) {
+                         this.state.controls.username.value
+                    }", isAdmin:${
+                         this.state.controls.isAdminField
+                              ? this.state.controls.isAdminField.value
+                              : null
+                    }, newPassword: "${this.state.controls.password.value}") {
                 username
               }
             }
-          `
-        })
-      )
-      .then(data => {
-        this.props.pushFunc("/");
-        this.props.pushFunc("/user/" + this.props.userId);
+          `,
+               }),
+          ).then(() => {
+               if (this.props.authUserId === this.props.userId) {
+                    localStorage.removeItem('token');
+                    window.location.assign('/');
+               }
+               this.props.reAuthFunc();
+               this.props.notifyFunc('User Successfully updated!');
+          });
+     };
 
-        this.props.reAuthFunc();
+     deleteUserHandler = () => {
+          const verify = window.confirm(
+               'Are you sure the you want to delete this user?',
+          );
 
-        this.props.notifyFunc("User Successfully updated!");
-      });
-  };
-
-  deleteUserHandler = () => {
-    const verify = window.confirm(
-      "Are you sure the you want to delete this user?"
-    );
-
-    if (verify) {
-      axios
-        .post(
-          `/?token=${localStorage.getItem("token")}`,
-          JSON.stringify({
-            query: `
+          if (verify) {
+               axios.post(
+                    `/?token=${localStorage.getItem('token')}`,
+                    JSON.stringify({
+                         query: `
           mutation {
             deleteUser(id: ${this.props.userId})
           }
-          `
-          })
-        )
-        .then(() => {
-          window.location.assign("/");
-          alert("User Deleted!");
-        });
-    }
-  };
+          `,
+                    }),
+               ).then(() => {
+                    window.location.assign('/');
+                    alert('User Deleted!');
+               });
+          }
+     };
 
-  render() {
-    const formElementArray = [];
+     render() {
+          const formElementArray = [];
 
-    for (let key in this.state.controls) {
-      formElementArray.push({
-        id: key,
-        config: this.state.controls[key]
-      });
-    }
+          for (let key in this.state.controls) {
+               formElementArray.push({
+                    id: key,
+                    config: this.state.controls[key],
+               });
+          }
 
-    let form = formElementArray.map(formElement => (
-      <Input
-        key={formElement.id}
-        elementType={formElement.config.elementType}
-        elementConfig={formElement.config.elementConfig}
-        value={formElement.config.value}
-        invalid={!formElement.config.valid}
-        shouldValidate={formElement.config.validation}
-        touched={formElement.config.touched}
-        changed={event => this.inputChangedHandler(event, formElement.id)}
-      />
-    ));
+          let form = formElementArray.map(formElement => (
+               <Input
+                    key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    invalid={!formElement.config.valid}
+                    shouldValidate={formElement.config.validation}
+                    touched={formElement.config.touched}
+                    changed={event =>
+                         this.inputChangedHandler(event, formElement.id)
+                    }
+               />
+          ));
 
-    return (
-      <div className={classes.EditUser}>
-        <form onSubmit={this.submitHandler}>
-          <h1>Edit User</h1>
-          {!this.props.loading ? form : <Spinner />}
-          <Button btnType="Primary">SUBMIT</Button>
-          {this.props.isAdmin && (
-            <Button btnType="Danger" clicked={this.deleteUserHandler}>
-              DELETE USER
-            </Button>
-          )}
-        </form>
-      </div>
-    );
-  }
+          return (
+               <div className={classes.EditUser}>
+                    <form onSubmit={this.submitHandler}>
+                         <h1>Edit User</h1>
+                         {!this.props.loading ? (
+                              <React.Fragment>
+                                   {form}
+                                   <Button btnType='Primary'>SUBMIT</Button>
+                                   {this.props.isAdmin && (
+                                        <Button
+                                             btnType='Danger'
+                                             clicked={this.deleteUserHandler}
+                                        >
+                                             DELETE USER
+                                        </Button>
+                                   )}
+                              </React.Fragment>
+                         ) : (
+                              <Spinner />
+                         )}
+                    </form>
+               </div>
+          );
+     }
 }
 
-export default EditUser;
+const mapStateToProps = state => ({
+     authUserId: state.auth.user.id,
+});
+
+export default connect(mapStateToProps)(EditUser);
--- src/views/User/ViewUser/ViewUser.js
+++ src/views/User/ViewUser/ViewUser.js
@@ -1,103 +1,111 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import classes from "./ViewUser.module.css";
+import classes from './ViewUser.module.css';
 
-import * as actions from "../../../store/actions";
+import * as actions from '../../../store/actions';
 
-import authRequired from "../../../hoc/authRequired/authRequired";
+import authRequired from '../../../hoc/authRequired/authRequired';
 
-import Spinner from "../../../components/UI/Spinner/Spinner";
-import Button from "../../../components/UI/Button/Button";
-import Modal from "../../../components/UI/Modal/Modal";
-import EditUser from "../../../components/Users/EditUser/EditUser";
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import Button from '../../../components/UI/Button/Button';
+import Modal from '../../../components/UI/Modal/Modal';
+import EditUser from '../../../components/Users/EditUser/EditUser';
 
 class ViewUser extends Component {
-  state = {
-    showEditModal: false
-  };
+     state = {
+          showEditModal: false,
+     };
 
-  constructor(props) {
-    super(props);
-    if (props.match.params.id === "current-user") {
-      props.initUser(props.currentUser.id);
-    } else {
-      props.initUser(props.match.params.id);
-    }
-  }
+     constructor(props) {
+          super(props);
+          if (props.match.params.id === 'current-user') {
+               props.initUser(props.currentUser.id);
+          } else {
+               props.initUser(props.match.params.id);
+          }
+     }
 
-  toggleEditModalHandler = () => {
-    this.setState(prvState => ({
-      showEditModal: !prvState.showEditModal
-    }));
-  };
+     toggleEditModalHandler = () => {
+          this.setState(prvState => ({
+               showEditModal: !prvState.showEditModal,
+          }));
+     };
 
-  render() {
-    return !this.props.loading ? (
-      <main className={classes.ViewUser}>
-        <h1 className={classes.Title}>
-          {this.props.user.username}
-          {this.props.user.isAdmin && (
-            <i className="material-icons">star_rate</i>
-          )}
-        </h1>
-        {(this.props.currentUser.isAdmin ||
-          this.props.match.params.id === "current-user") && (
-          <Button btnType="Primary" clicked={this.toggleEditModalHandler}>
-            EDIT PROFILE
-          </Button>
-        )}
-        {this.props.match.params.id === "current-user" && (
-          <Button
-            btnType="Secondary"
-            clicked={() => this.props.logoutHandler(this.props.history.push)}
-          >
-            LOGOUT
-          </Button>
-        )}
+     render() {
+          return !this.props.loading ? (
+               <main className={classes.ViewUser}>
+                    <h1 className={classes.Title}>
+                         {this.props.user.username}
+                         {this.props.user.isAdmin && (
+                              <i className='material-icons'>star_rate</i>
+                         )}
+                    </h1>
+                    {(this.props.currentUser.isAdmin ||
+                         this.props.match.params.id === 'current-user') && (
+                         <Button
+                              btnType='Primary'
+                              clicked={this.toggleEditModalHandler}
+                         >
+                              EDIT PROFILE
+                         </Button>
+                    )}
+                    {this.props.match.params.id === 'current-user' && (
+                         <Button
+                              btnType='Secondary'
+                              clicked={() =>
+                                   this.props.logoutHandler(
+                                        this.props.history.push,
+                                   )
+                              }
+                         >
+                              LOGOUT
+                         </Button>
+                    )}
 
-        <Modal
-          size={this.props.currentUser.isAdmin ? 4.5 : 3.0}
-          scroll={false}
-          show={this.state.showEditModal}
-          modalClosed={this.toggleEditModalHandler}
-        >
-          {this.props.user.username && (
-            <EditUser
-              notifyFunc={this.props.notify}
-              pushFunc={this.props.history.push}
-              reAuthFunc={this.props.reAuth}
-              userId={
-                this.props.match.params.id === "current-user"
-                  ? this.props.currentUser.id
-                  : this.props.match.params.id
-              }
-              currentUser={this.props.user}
-              isAdmin={this.props.currentUser.isAdmin}
-            />
-          )}
-        </Modal>
-      </main>
-    ) : (
-      <Spinner />
-    );
-  }
+                    <Modal
+                         size={4.5}
+                         show={this.state.showEditModal}
+                         modalClosed={this.toggleEditModalHandler}
+                         scroll
+                    >
+                         {this.props.user.username && (
+                              <EditUser
+                                   notifyFunc={this.props.notify}
+                                   pushFunc={this.props.history.push}
+                                   reAuthFunc={this.props.reAuth}
+                                   userId={
+                                        this.props.match.params.id ===
+                                        'current-user'
+                                             ? this.props.currentUser.id
+                                             : this.props.match.params.id
+                                   }
+                                   currentUser={this.props.user}
+                                   isAdmin={this.props.currentUser.isAdmin}
+                              />
+                         )}
+                    </Modal>
+               </main>
+          ) : (
+               <Spinner />
+          );
+     }
 }
 
 const mapStateToProps = state => ({
-  currentUser: state.auth.user,
-  loading: state.users.loading,
-  user: state.users.user
+     currentUser: state.auth.user,
+     loading: state.users.loading,
+     user: state.users.user,
 });
 
 const mapDispatchToProps = dispatch => ({
-  logoutHandler: goFunc => dispatch(actions.logout(goFunc)),
-  reAuth: () => dispatch(actions.auth(actions.isLoggedIn())),
-  notify: message => dispatch(actions.notify({ message: message })),
-  initUser: userId => dispatch(actions.initUser(userId))
+     logoutHandler: goFunc => dispatch(actions.logout(goFunc)),
+     reAuth: () => dispatch(actions.auth(actions.isLoggedIn())),
+     notify: message => dispatch(actions.notify({ message: message })),
+     initUser: userId => dispatch(actions.initUser(userId)),
 });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+     mapStateToProps,
+     mapDispatchToProps,
 )(authRequired(ViewUser));
