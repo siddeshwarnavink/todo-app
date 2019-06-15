8a42868f98418a246ac927c6854db36afaa08a03
Siddeshwar <jjsiddeshwar61@gmail.com>
2019-06-15 19:05:22 +0530

    Updated desktop UI

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,21 +1,22 @@
-import React, { Component, Suspense } from 'react';
-import { connect } from 'react-redux';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
-import { BrowserView, MobileView } from 'react-device-detect';
-import { isArray } from 'util';
+import React, { Component, Suspense } from "react";
+import { connect } from "react-redux";
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
+import { BrowserView, MobileView } from "react-device-detect";
+import { isArray } from "util";
 
-import * as actions from '../store/actions';
-import authRequired from '../hoc/authRequired/authRequired';
-import Fab from '../components/UI/Button/Fab/Fab';
-import Modal from '../components/UI/Modal/Modal';
-import CreateGroupTask from '../components/Tasks/CreateGroupTask/CreateGroupTask';
-import GroupCard from '../components/Groups/GroupCard/GroupCard';
-import Spinner from '../components/UI/Spinner/Spinner';
-import TasksList from '../components/Tasks/TasksList/TasksList';
-import Notificatons from './Notification/Notification';
-import NotificationContext from '../context/notification-context';
+import * as actions from "../store/actions";
+import authRequired from "../hoc/authRequired/authRequired";
+import Fab from "../components/UI/Button/Fab/Fab";
+import Modal from "../components/UI/Modal/Modal";
+import CreateGroupTask from "../components/Tasks/CreateGroupTask/CreateGroupTask";
+import GroupCard from "../components/Groups/GroupCard/GroupCard";
+import Spinner from "../components/UI/Spinner/Spinner";
+import TasksList from "../components/Tasks/TasksList/TasksList";
+import Notificatons from "./Notification/Notification";
+import NotificationContext from "../context/notification-context";
+import Jumbotron from "../components/UI/Jumbotron/Jumbotron";
 const MobileJumbotron = React.lazy(() =>
-     import('../components/UI/Jumbotron/MobileJumbotron/MobileJumbotron'),
+     import("../components/UI/Jumbotron/MobileJumbotron/MobileJumbotron"),
 );
 
 class Homepage extends Component {
@@ -41,9 +42,9 @@ class Homepage extends Component {
           let hrs = new Date().getHours();
           let greet;
 
-          if (hrs < 12) greet = 'Good Morning';
-          else if (hrs >= 12 && hrs <= 17) greet = 'Good Afternoon';
-          else if (hrs >= 17 && hrs <= 24) greet = 'Good Evening';
+          if (hrs < 12) greet = "Good Morning";
+          else if (hrs >= 12 && hrs <= 17) greet = "Good Afternoon";
+          else if (hrs >= 17 && hrs <= 24) greet = "Good Evening";
 
           let groups = this.props.groupsError ? (
                <p>Failed to load the groups</p>
@@ -84,7 +85,7 @@ class Homepage extends Component {
                               }}
                          />
                     </Modal>
-                    <BrowserView>
+                    {/* <BrowserView>
                          <main>
                               {isArray(this.props.todos) ? (
                                    this.props.todos.length > 0 ? (
@@ -119,82 +120,86 @@ class Homepage extends Component {
                          >
                               <i className='material-icons'>add</i>
                          </Fab>
+                    </BrowserView> */}
+                    {/* <MobileView> */}
+                    <BrowserView>
+                         <Jumbotron subtext={new Date().toDateString()}>
+                              <h1>{greet}</h1>
+                         </Jumbotron>
                     </BrowserView>
                     <MobileView>
                          <MobileJumbotron subtext={new Date().toDateString()}>
                               <h1>{greet}</h1>
                          </MobileJumbotron>
-
-                         <Tabs>
-                              <TabList>
-                                   <Tab>
-                                        <i className='material-icons'>home</i>
-                                        <span>Home</span>
-                                   </Tab>
-                                   <Tab>
-                                        <i className='material-icons'>group</i>
-                                        <span>Groups</span>
-                                   </Tab>
-                                   <Tab>
-                                        <NotificationContext.Consumer>
-                                             {notificationCount => (
-                                                  <>
-                                                       <i className='material-icons'>
-                                                            {notificationCount ===
-                                                            0
-                                                                 ? 'notifications_none'
-                                                                 : 'notifications_active'}
-                                                       </i>
-                                                       <span>Notification</span>
-                                                  </>
-                                             )}
-                                        </NotificationContext.Consumer>
-                                   </Tab>
-                              </TabList>
-                              <TabPanel>
-                                   {isArray(this.props.todos) ? (
-                                        this.props.todos.length > 0 ? (
-                                             <React.Fragment>
-                                                  <h1 className='mobile-sub'>
-                                                       Things Todo!
-                                                  </h1>
-
-                                                  <TasksList
-                                                       isTodo
-                                                       tasks={this.props.todos}
-                                                       extFunc={() => {
-                                                            this.props.initTodos();
-                                                       }}
-                                                  />
-                                             </React.Fragment>
-                                        ) : null
-                                   ) : (
-                                        <Spinner />
-                                   )}
-
-                                   <h1 className='mobile-sub'>
-                                        Tasks Assigned
-                                   </h1>
-                                   {!this.props.tasksLoading ? (
-                                        <TasksList tasks={this.props.tasks} />
-                                   ) : (
-                                        <Spinner />
-                                   )}
-
-                                   <Fab
-                                        tooltip='Add Task'
-                                        clicked={this.toggleAddTaskModel}
-                                   >
-                                        <i className='material-icons'>add</i>
-                                   </Fab>
-                              </TabPanel>
-                              <TabPanel>{groups}</TabPanel>
-
-                              <TabPanel>
-                                   <Notificatons />
-                              </TabPanel>
-                         </Tabs>
                     </MobileView>
+
+                    <Tabs>
+                         <TabList>
+                              <Tab>
+                                   <i className='material-icons'>home</i>
+                                   <span>Home</span>
+                              </Tab>
+                              <Tab>
+                                   <i className='material-icons'>group</i>
+                                   <span>Groups</span>
+                              </Tab>
+                              <Tab>
+                                   <NotificationContext.Consumer>
+                                        {notificationCount => (
+                                             <>
+                                                  <i className='material-icons'>
+                                                       {notificationCount === 0
+                                                            ? "notifications_none"
+                                                            : "notifications_active"}
+                                                  </i>
+                                                  <span>Notification</span>
+                                             </>
+                                        )}
+                                   </NotificationContext.Consumer>
+                              </Tab>
+                         </TabList>
+                         <TabPanel>
+                              {isArray(this.props.todos) ? (
+                                   this.props.todos.length > 0 ? (
+                                        <React.Fragment>
+                                             <h1 className='mobile-sub'>
+                                                  Things Todo!
+                                             </h1>
+
+                                             <TasksList
+                                                  isTodo
+                                                  tasks={this.props.todos}
+                                                  extFunc={() => {
+                                                       this.props.initTodos();
+                                                  }}
+                                             />
+                                        </React.Fragment>
+                                   ) : null
+                              ) : (
+                                   <Spinner />
+                              )}
+
+                              <h1 className='mobile-sub'>Tasks Assigned</h1>
+                              {!this.props.tasksLoading ? (
+                                   <TasksList tasks={this.props.tasks} />
+                              ) : (
+                                   <Spinner />
+                              )}
+
+                              <Fab
+                                   tooltip='Add Task'
+                                   clicked={this.toggleAddTaskModel}
+                              >
+                                   <i className='material-icons'>add</i>
+                              </Fab>
+                         </TabPanel>
+                         <TabPanel>{groups}</TabPanel>
+
+                         <TabPanel>
+                              <Notificatons />
+                         </TabPanel>
+                    </Tabs>
+                    {/* </MobileView> */}
                </Suspense>
           );
      }
